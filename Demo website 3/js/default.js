5681ca5eaf2ae48e090ab40db9e2f86c081dea1a
Madalina Rosca <Madalina Rosca>
2016-12-07 16:02:11 +0200

    e-mail template, jpg previews, improvements

--- Demo website 3/js/default.js
+++ Demo website 3/js/default.js
@@ -36,3 +36,16 @@ function check_if_in_view() {
 $window.on('scroll', check_if_in_view);
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('#hide_me').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it in */
+            if( bottom_of_window >= bottom_of_object ) {
+                $(this).animate({'opacity':'1'},1000);
+            }
+        }); 
+    });
+});
